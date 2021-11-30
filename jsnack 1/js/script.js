9a1ec9fe5ce33951066a57bbfe058d85b2dcc3e7
frame88 <fleuzzi97@gmail.com>
2021-11-30 11:20:23 +0100

    finish!

--- jsnack 1/js/script.js
+++ jsnack 1/js/script.js
@@ -1,24 +1,26 @@
-let cognomi = ['rossi','bianchi','venditti','carlucci','wasozki'];
-let nomi = ['alessio','myke','luis','jhonatan','loris'];
-
-function false(par1) {
-    let falsalista = [];
-    for (let i = 0; i < cognomi.length; i++) {
-        let i = Math.floor(Math.random() * (cognomi.length - 1)) + 1;
-        let j = Math.floor(Math.random() * (cognomi.length - 1)) + 1;
-        falsalista.push(cognomi[i] + ' ' + nomi[j]);
-        return falsalista;
-    } 
-}
-
-console.log(falsalista);
-
-
-
-
 /*
 jsnack 1
 Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
 lista di cognomi, 
 Gatsby vuole generare una falsa lista di invitati.
 */
+
+
+
+let cognomi = ['rossi','bianchi','venditti','carlucci','wasozki'];
+let nomi = ['alessio','myke','luis','jhonatan','loris'];
+let newlist = falselist(cognomi, nomi);
+
+function falselist(par1, par2) {
+    let falsalista = [];
+    for (let i = 0; i < par1.length; i++) {
+        let i = Math.floor(Math.random() * (par1.length - 1)) + 1;
+        let j = Math.floor(Math.random() * (par1.length - 1)) + 1;
+        falsalista.push(par1[i] + ' ' + par2[j]);
+    } 
+    return falsalista;
+}
+
+console.log(newlist);
+
+
