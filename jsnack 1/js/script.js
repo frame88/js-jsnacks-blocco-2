a51eace5f6a441daf6b8b6aa6152a670c58d320b
frame88 <fleuzzi97@gmail.com>
2021-11-30 11:03:12 +0100

    finish?

--- jsnack 1/js/script.js
+++ jsnack 1/js/script.js
@@ -1,14 +1,19 @@
 let cognomi = ['rossi','bianchi','venditti','carlucci','wasozki'];
 let nomi = ['alessio','myke','luis','jhonatan','loris'];
-let falsalista = [];
 
-for (let i = 0; i < cognomi.length; i++) {
-    let i = Math.floor(Math.random() * cognomi.length) + 1;
-    let j = Math.floor(Math.random() * cognomi.length) + 1;
-    falsalista.push(cognomi[i] + nomi [j]);
+function false(par1) {
+    let falsalista = [];
+    for (let i = 0; i < cognomi.length; i++) {
+        let i = Math.floor(Math.random() * (cognomi.length - 1)) + 1;
+        let j = Math.floor(Math.random() * (cognomi.length - 1)) + 1;
+        falsalista.push(cognomi[i] + ' ' + nomi[j]);
+        return falsalista;
+    } 
 }
 
 console.log(falsalista);
+
+
 
 
 /*
